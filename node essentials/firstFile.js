647d56041b1d38d2612ce3f4d74cce69723aa65b
Jason Huang <jashuang1130@gmail.com>
2020-04-24 11:31:14 -0400

    add es6_basics to repo

--- node essentials/firstFile.js
+++
@@ -1,4 +0,0 @@
-let hello = "Hello World From Node JS";
-
-console.log(hello);
-// Global object https://nodejs.org/api/globals.html
--- node essentials/global.js
+++
@@ -1,5 +0,0 @@
-// require()) loads external modules 
-
-const path = require("path")
-console.log(`The file name is ${path.basename(__filename)}`)
-
